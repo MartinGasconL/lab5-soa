c17dd77b5123b72f5ab64ce9e5e16a173675681e
764429 <764429@unizar.es>
2020-12-11 12:19:21 +0100

    Implemented max functionality.

--- src/main/java/soa/eip/Router.java
+++ src/main/java/soa/eip/Router.java
@@ -1,5 +1,7 @@
 package soa.eip;
 
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
 import org.springframework.stereotype.Component;
 
@@ -11,9 +13,31 @@ public class Router extends RouteBuilder {
   @Override
   public void configure() {
     from(DIRECT_URI)
-      .log("Body contains \"${body}\"")
-      .log("Searching twitter for \"${body}\"!")
-      .toD("twitter-search:${body}")
-      .log("Body now contains the response from twitter:\n${body}");
+            .log("Body contains \"${body}\"")
+            .log("Searching twitter for \"${body}\"!")
+            .process(new MaxProcessor())
+            .toD("twitter-search:${body}")
+            .log("Body now contains the response from twitter:\n${body}");
+  }
+
+  private static class MaxProcessor implements Processor {
+    public void process(Exchange exchange) {
+      String message = exchange.getIn().getBody(String.class);
+      String[] words = message.split(" ");
+      StringBuilder body = new StringBuilder();
+      String max = "";
+      for(String word : words){
+        if(word.matches("max:[0-9]+")){
+          max = word.substring(4);
+        }else {
+            body.append(" ").append(word);
+        }
+      }
+      if(!max.equals("")){
+        body.append("?count=").append(max);
+      }
+
+      exchange.getOut().setBody(body);
+    }
   }
 }
